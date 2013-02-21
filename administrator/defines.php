bfbc08f3eac2bb9931bc17e6034ed89d6cbd2609
Rick Deckard <xxxblasteroid@gmail.com>
2013-02-21 10:03:07 +0400

    fix error_log

--- administrator/defines.php
+++ administrator/defines.php
@@ -1,6 +1,6 @@
 <?php
 
-error_reporting(E_ALL);
+error_reporting(30719); // E_ALL ^ E_STRICT = 30719
 ini_set('display_errors', 0);
 ini_set('log_errors', 1);
 ini_set('error_log', dirname(__FILE__) . '/../logs/error_log');
